f862d730a93180883484146167d8b9d317b4c705
Teja Škvarč <teja.skvarc@gmail.com>
2018-06-02 11:45:16 +0200

    Dodala funkcijo equals v Potezo. Preverila delovanje funkcije moznePoteze in veljavnaPoteza (glej TestIgra)

--- Less/src/igra/Poteza.java
+++ Less/src/igra/Poteza.java
@@ -21,5 +21,24 @@ public class Poteza {
 	public Lokacija getKoncna() {
 		return koncna;
 	}
+
+
+	@Override
+	public boolean equals(Object arg0) {
+		if (this == arg0) {
+			return true;
+		}
+		if (arg0 == null) {
+			return false;
+		}
+		if (getClass() != arg0.getClass()) {
+			return false;
+		}
+		Poteza other = (Poteza) arg0;
+		if (this.zacetna.equals(other.zacetna) && this.koncna.equals(other.koncna)) {
+			return true;
+		}
+		return false;
+	}
 	
 }
--- Less/src/test/TestIgra.java
+++ Less/src/test/TestIgra.java
@@ -1,6 +1,8 @@
 package test;
 
 
+import java.util.LinkedList;
+
 import igra.Igra;
 import igra.Lokacija;
 import igra.Poteza;
@@ -12,8 +14,18 @@ public class TestIgra extends TestCase {
 		Igra igra = new Igra();
 		//assertEquals(igra.veljavnaPoteza(zacetna,koncna), true);
 		Igra igra2 = new Igra(igra);
-		igra2.narediPotezo(new Poteza(new Lokacija(1,1), new Lokacija(1,2)));
+		//igra2.narediPotezo(new Poteza(new Lokacija(1,1), new Lokacija(1,2)));
 		//assertEquals(igra.getNaPotezi(),igra2.getNaPotezi() );
-		assertEquals(igra.getIgralnaPlosca(), igra2.getIgralnaPlosca());
+		//assertEquals(igra.getIgralnaPlosca(), igra2.getIgralnaPlosca());
+		Poteza poteza = new Poteza(new Lokacija(1,1), new Lokacija(1,2));
+		LinkedList<Poteza> mozne = igra.moznePoteze(poteza.getZacetna(), igra.getKvotaPremikov());
+		for (Poteza p : mozne) {
+			System.out.println("Zacetna: " );
+			p.getZacetna().print();
+			System.out.print("Koncna: ");
+			p.getKoncna().print();
+		}
+		assertEquals(igra.veljavnaPoteza(poteza), true);
 	}
+	
 }
